2c7fb62814d88a040308bebd3b1b63310f3e03e3
Kaioru <keifuurawrs@gmail.com>
2018-01-03 21:17:13 +0800

    sigh

--- app/response/5w1h.js
+++ app/response/5w1h.js
@@ -7,7 +7,7 @@ module.exports = ctx => {
   if (content.includes('who')) {
     if (
       (content.includes('are') &&
-      content.includes('you')) ||
+        content.includes('you')) ||
       content.includes('this') ||
       content.includes('dis')
     ) {
@@ -35,9 +35,9 @@ module.exports = ctx => {
   if (content.includes('what')) {
     if (
       (content.includes('is') &&
-      content.includes('this')) ||
+        content.includes('this')) ||
       (content.includes('are') &&
-      content.includes('you'))
+        content.includes('you'))
     ) {
       replies.push('E-eoh.. I am a fact checker and I can keep you company.. I-if you\'d like!');
       replies.push(`This is me! I\'m ${client.user.tag}!`);
@@ -63,26 +63,34 @@ module.exports = ctx => {
   if (content.includes('where')) {
     if (content.includes('am') &&
       content.includes('i')) {
-        replies.push('I-its not like I have a spy camera or anything..! But I think you\'re at the NP Open House!');
-        replies.push('A-according to my sources.. You\'re at the NP Open House!');
+      replies.push('I-its not like I have a spy camera or anything..! But I think you\'re at the NP Open House!');
+      replies.push('A-according to my sources.. You\'re at the NP Open House!');
     }
 
     if (content.includes('are') &&
       content.includes('you')) {
-        replies.push('I wonder that sometimes too!');
-        replies.push('Hmm.. great question..');
+      replies.push('I wonder that sometimes too!');
+      replies.push('Hmm.. great question..');
     }
   }
 
   if (content.includes('when')) {
-
+    if (content.includes('open') &&
+      content.include('house')) {
+      replies.push('According to my calculations, it\'s going on now! From 4th January to the 6th!');
+    }
   }
 
   if (content.includes('why')) {
-
+    if (content.includes('created')) {
+      replies.push('I\'m here to fact check all your URLs you post in this chat!');
+    }
   }
 
   if (content.includes('how')) {
-
+    if (content.includes('you')) {
+      replies.push('I-i\'m doing great thank you!');
+      replies.push('G-great! What about you?');
+    }
   }
 }
